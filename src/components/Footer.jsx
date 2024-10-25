a0c8ac12f6fea6a54c8641dfd04a0c70b9743f79
risyadkamarullahsolecode1 <risyad.kamarullah@solecode.id>
2024-10-25 18:16:36 +0700

    update styling in header and footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,22 +1,23 @@
 import React from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';  // Import Bootstrap CSS
+import 'bootstrap/dist/js/bootstrap.bundle.min'; // Import Bootstrap JS if needed
 
 const Footer = () => {
   return (
-    <footer className="bg-danger py-4 mt-auto">
-      <Container>
-        <Row className="align-items-center justify-content-between flex-column flex-sm-row">
-          <Col className="col-auto text-white small m-0">Copyright &copy; Restaurant Website 2024</Col>
-          <Col className="col-auto">
-            <a className="text-white small" href="https://www.facebook.com">Facebook</a>
-            <span className="text-white mx-1">&middot;</span>
-            <a className="text-white small" href="https://www.instagram.com">Instagram</a>
-            <span className="text-white mx-1">&middot;</span>
-            <a className="text-white small" href="https://www.twitter.com">Twitter</a>
-          </Col>
-        </Row>
-      </Container>
-    </footer>
+    <footer class="bg-danger py-4 mt-auto fixed-bottom">
+            <div class="container px-5">
+                <div class="row align-items-center justify-content-between flex-column flex-sm-row">
+                    <div class="col-auto"><div class="small m-0 text-white">Copyright &copy; Restaurant Website 2024</div></div>
+                    <div class="col-auto">
+                        <a class="link-light small" href="https://www.facebook.com">Facebook</a>
+                        <span class="text-white mx-1">&middot;</span>
+                        <a class="link-light small" href="https://www.instagram.com">Instagram</a>
+                        <span class="text-white mx-1">&middot;</span>
+                        <a class="link-light small" href="https://www.twitter.com">Twitter</a>
+                    </div>
+                </div>
+            </div>
+        </footer>
   );
 };
 
